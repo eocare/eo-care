2bac30d4eb32854eb77099ded350532d6de746f0
harishrongala <harishrongala@gmail.com>
2022-06-24 15:51:37 +0530

    home and age gate scripts added

---
+++ ageGate.js
@@ -0,0 +1,63 @@
+const ageGateBtnYes = document.getElementById('age-gate-btn-yes');
+const ageGateBtnNo = document.getElementById('age-gate-btn-no');
+
+ageGateBtnYes.onclick = function (e) {
+    e.target.dataset.selected = true;
+    ageGateBtnNo.dataset.selected = false;
+    e.target.style.backgroundColor = "#B4F9C7";
+}
+
+ageGateBtnNo.onclick = function (e) {
+    e.target.dataset.selected = true;
+    ageGateBtnYes.dataset.selected = false;
+    ageGateBtnYes.style.backgroundColor = "";
+}
+
+function showAgeRestrictionModal() {
+    document.getElementById('age-gate-form').style.display = 'none';
+    document.getElementById('age-restriction-info-div').style.display = 'block';
+    document.getElementById('ineligible-zip-div').style.display = 'none';
+}
+
+function showIneligibleZipModal() {
+    document.getElementById('age-gate-form').style.display = 'none';
+    document.getElementById('age-restriction-info-div').style.display = 'none';
+    document.getElementById('ineligible-zip-div').style.display = 'block';
+}
+
+const ageGateFormSubmitBtn = document.getElementById('age-gate-form-submit');
+ageGateFormSubmitBtn.onclick = async function (e) {
+    e.preventDefault();
+    if (ageGateBtnYes.dataset.selected === 'true') {
+        const ageGateZip = document.getElementById('age-gate-inp-zip').value;
+        let zipEligible = await isZipEligible(ageGateZip);
+        if (zipEligible) {
+            Cookies.set('validAge', true, { expires: 365 });
+            window.location.replace('/home-wip');
+        } else {
+            showIneligibleZipModal();
+        }
+    } else {
+        showAgeRestrictionModal();
+    }
+}
+
+async function isZipEligible(zip) {
+    const API_ROOT_DOMAIN = 'https://api.staging.eo.care';
+    const resp = await fetch(`${API_ROOT_DOMAIN}/profile/eligible`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "zip": zip
+        })
+    });
+
+    if (resp.ok && resp.status === 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
