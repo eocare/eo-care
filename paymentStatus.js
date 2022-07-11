8a7f66020644f4c57920f919d5619e65c97da510
harishrongala <harishrongala@gmail.com>
2022-07-11 21:24:41 +0530

    Payment status script added

--- paymentStatus.js
+++ paymentStatus.js
@@ -5,6 +5,12 @@ function paymentFailed() {
     document.querySelector('.payment-successful').style.display = 'none';
 }
 
+function paymentSuccessful() {
+    document.getElementById('payment-status-heading').innerText = 'Payment successful. Thank you!';
+    document.getElementById('payment-status-sub-heading').innerHTML = 'Adding a medical card? Our member support team will email you within 24 hours.'
+    document.querySelector('.payment-successful').style.display = 'flex';
+}
+
 function pageExpired() {
     document.getElementById('payment-status-heading').innerText = 'Sorry, this link is invalid or expired.';
     document.getElementById('payment-status-sub-heading').innerHTML = 'Need help? Call <a href="tel:877-707-0706">877-707-0706</a> or email us at <a href="mailto:support@eo.care">support@eo.care</a>.'
@@ -15,7 +21,9 @@ function pageExpired() {
 const qs = new URLSearchParams(document.location.search);
 if (qs.get('status')) {
     let status = qs.get('status');
-    if (status !== 'success') {
+    if (status === 'success') {
+        paymentSuccessful();
+    } else {
         paymentFailed();
     }
 } else {
