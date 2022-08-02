0099b8a244f7db1e7c0e889a247accdf7b8c7f7d
harishrongala <harishrongala@gmail.com>
2022-08-02 23:09:26 +0530

    address form field validation added

--- home.js
+++ home.js
@@ -11,47 +11,47 @@ if (Math.random() <= 0.50) {
 	heroImageBlock.style.backgroundImage = femaleHeroURL;
 }
 
-let animationOneIntervalId;
-let animationTwoIntervalId;
-let animationThreeIntervalId;
-document.querySelector('#animate1').style.opacity = '0';
-document.querySelector('#animate2').style.opacity = '1';
-document.querySelector('#animate3').style.opacity = '0';
-document.querySelector('#animate4').style.opacity = '1';
-document.querySelector('#animate5').style.opacity = '0';
-document.querySelector('#animate6').style.opacity = '1';
-window.onscroll = function (e) {
-	if (window.scrollY >= 871) {
-		// Begin animation
-		if (!animationOneIntervalId) {
-			animationOneIntervalId = setInterval(()=>{
-				let secOneSlideOneOpacity = document.querySelector('#animate1').style.opacity;
-				let secOneSlideTwoOpacity = document.querySelector('#animate2').style.opacity;
-				document.querySelector('#animate1').style.opacity = String(Number(!Boolean(Number(secOneSlideOneOpacity))));
-				document.querySelector('#animate2').style.opacity = String(Number(!Boolean(Number(secOneSlideTwoOpacity))));
-			}, 2500);
-		}
-	}
-	if (window.scrollY >= 871) {
-		// Begin animation
-		if (!animationTwoIntervalId) {
-			animationTwoIntervalId = setInterval(()=>{
-				let secTwoSlideOneOpacity = document.querySelector('#animate3').style.opacity;
-				let secTwoSlideTwoOpacity = document.querySelector('#animate4').style.opacity;
-				document.querySelector('#animate3').style.opacity = String(Number(!Boolean(Number(secTwoSlideOneOpacity))));
-				document.querySelector('#animate4').style.opacity = String(Number(!Boolean(Number(secTwoSlideTwoOpacity))));
-			}, 2500);
-		}
-	}
-	if (window.scrollY >= 871) {
-		// Begin animation
-		if (!animationThreeIntervalId) {
-			animationThreeIntervalId = setInterval(()=>{
-				let secThreeSlideOneOpacity = document.querySelector('#animate5').style.opacity;
-				let secThreeSlideTwoOpacity = document.querySelector('#animate6').style.opacity;
-				document.querySelector('#animate5').style.opacity = String(Number(!Boolean(Number(secThreeSlideOneOpacity))));
-				document.querySelector('#animate6').style.opacity = String(Number(!Boolean(Number(secThreeSlideTwoOpacity))));
-			}, 2500);
-		}
-	}
-};
+// let animationOneIntervalId;
+// let animationTwoIntervalId;
+// let animationThreeIntervalId;
+// document.querySelector('#animate1').style.opacity = '0';
+// document.querySelector('#animate2').style.opacity = '1';
+// document.querySelector('#animate3').style.opacity = '0';
+// document.querySelector('#animate4').style.opacity = '1';
+// document.querySelector('#animate5').style.opacity = '0';
+// document.querySelector('#animate6').style.opacity = '1';
+// window.onscroll = function (e) {
+// 	if (window.scrollY >= 871) {
+// 		// Begin animation
+// 		if (!animationOneIntervalId) {
+// 			animationOneIntervalId = setInterval(()=>{
+// 				let secOneSlideOneOpacity = document.querySelector('#animate1').style.opacity;
+// 				let secOneSlideTwoOpacity = document.querySelector('#animate2').style.opacity;
+// 				document.querySelector('#animate1').style.opacity = String(Number(!Boolean(Number(secOneSlideOneOpacity))));
+// 				document.querySelector('#animate2').style.opacity = String(Number(!Boolean(Number(secOneSlideTwoOpacity))));
+// 			}, 2500);
+// 		}
+// 	}
+// 	if (window.scrollY >= 871) {
+// 		// Begin animation
+// 		if (!animationTwoIntervalId) {
+// 			animationTwoIntervalId = setInterval(()=>{
+// 				let secTwoSlideOneOpacity = document.querySelector('#animate3').style.opacity;
+// 				let secTwoSlideTwoOpacity = document.querySelector('#animate4').style.opacity;
+// 				document.querySelector('#animate3').style.opacity = String(Number(!Boolean(Number(secTwoSlideOneOpacity))));
+// 				document.querySelector('#animate4').style.opacity = String(Number(!Boolean(Number(secTwoSlideTwoOpacity))));
+// 			}, 2500);
+// 		}
+// 	}
+// 	if (window.scrollY >= 871) {
+// 		// Begin animation
+// 		if (!animationThreeIntervalId) {
+// 			animationThreeIntervalId = setInterval(()=>{
+// 				let secThreeSlideOneOpacity = document.querySelector('#animate5').style.opacity;
+// 				let secThreeSlideTwoOpacity = document.querySelector('#animate6').style.opacity;
+// 				document.querySelector('#animate5').style.opacity = String(Number(!Boolean(Number(secThreeSlideOneOpacity))));
+// 				document.querySelector('#animate6').style.opacity = String(Number(!Boolean(Number(secThreeSlideTwoOpacity))));
+// 			}, 2500);
+// 		}
+// 	}
+// };
