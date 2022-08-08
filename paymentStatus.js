c4eadde7a1cfd1983b4e4761885d8141f4570372
harishrongala <harishrongala@gmail.com>
2022-08-08 21:29:25 +0530

    production api

--- paymentStatus.js
+++ paymentStatus.js
@@ -21,7 +21,7 @@ function paymentFailed() {
 function paymentSuccessful(selectedPlan, userEmailAddress) {
     // Set Dynamic text
     if (selectedPlan && userEmailAddress) {
-        let selectedPlanAmount = selectedPlan == 'monthly' ? '$39' : '$249';
+        let selectedPlanAmount = selectedPlan == 'plan_monthly' ? '$39' : '$249';
         document.getElementById('payment-success-status-sub-heading').innerText = `Thanks for subscribing. A bill for ${selectedPlanAmount} will be sent shortly to ${atob(userEmailAddress)}. Adding a medical card? Our support team will email you within 24 hours.`;
     }
 
