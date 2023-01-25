1ad80a1571bf52b82aa84b5e25ce6d04eb2b1448
harishrongala <harishrongala@gmail.com>
2023-01-25 14:03:12 +0530

    log

--- NAGE/createAccount.js
+++ NAGE/createAccount.js
@@ -3,6 +3,7 @@ function onload() {
     const submission_id = qs.get('submission_id')
     document.getElementById('accountSetupIframe').src = document.getElementById('accountSetupIframe').src + `?submission_id=${submission_id}`
     // iFrame event listener
+    console.log(`Adding iFrame message listener.`)
     window.addEventListener('message', function(event) {
         console.log("Message received from the child: " + event.data); // Message received from child
     });
