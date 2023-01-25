6e6d6d091f4ffcacf0ba45bd0c2c8aa26da4deb9
harishrongala <harishrongala@gmail.com>
2023-01-25 17:52:01 +0530

    localstorage

--- NAGE/createAccount.js
+++ NAGE/createAccount.js
@@ -1,6 +1,8 @@
 function onload() {
     const qs = new URLSearchParams(window.location.search)
     const submission_id = qs.get('submission_id')
+    const medCardInterest = qs.get('med_card')
+    const thcPref = qs.get('thc_preference')
     const _iframeDivId = 'JotFormIFrame-230167209377155'
     document.getElementById(_iframeDivId).src = document.getElementById(_iframeDivId).src + `?submission_id=${submission_id}`
     // iFrame event listener
@@ -8,6 +10,17 @@ function onload() {
     window.addEventListener('message', function(event) {
         console.log("Message received from the child: ") // Message received from child
         console.log(event.data)
+        const { zip, uid, thc_type_preference, med_card } = event.data
+        if (zip) {
+            const { med_card_delivery_only, rec_delivery_possible } = zip
+            this.localStorage.setItem('med_card_delivery_only', med_card_delivery_only)
+            this.localStorage.setItem('rec_delivery_possible', rec_delivery_possible)
+        }
+        if (uid || thc_type_preference || med_card) {
+            this.localStorage.setItem('uid', uid)
+            this.localStorage.setItem('thc_type_preference', thc_type_preference)
+            this.localStorage.setItem('med_card', med_card)
+        }
     });
 }
 
