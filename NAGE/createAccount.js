d835eca4d0062dbc9485ffb658bf9286112b8ebe
harishrongala <harishrongala@gmail.com>
2023-01-25 14:07:04 +0530

    log

--- NAGE/createAccount.js
+++ NAGE/createAccount.js
@@ -1,7 +1,8 @@
 function onload() {
     const qs = new URLSearchParams(window.location.search)
     const submission_id = qs.get('submission_id')
-    document.getElementById('accountSetupIframe').src = document.getElementById('accountSetupIframe').src + `?submission_id=${submission_id}`
+    const _iframeDivId = 'JotFormIFrame-230167209377155'
+    document.getElementById(_iframeDivId).src = document.getElementById(_iframeDivId).src + `?submission_id=${submission_id}`
     // iFrame event listener
     console.log(`Adding iFrame message listener.`)
     window.addEventListener('message', function(event) {
