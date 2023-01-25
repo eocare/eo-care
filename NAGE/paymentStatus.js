d624d7652f475407a33fb3f7f44e5163ebc51f78
harishrongala <harishrongala@gmail.com>
2023-01-26 00:18:11 +0530

    thank you page

--- NAGE/paymentStatus.js
+++ NAGE/paymentStatus.js
@@ -87,6 +87,12 @@ function profileCompletionLink() {
     } else if (med_card_delivery_only == "false") {
         med_card_delivery_only = "False"
     }
+
+    if (thc_type_preference.toLowerCase().includes('thc')) {
+        thc_type_preference = 'THC'
+    } else {
+        thc_type_preference = 'CBD'
+    }
     const link = `https://form.jotform.com/230184858952466?thc_plan_type=${thc_type_preference}&med_card_status=${med_card}&rec_delivery_possible=${rec_delivery_possible}&med_delivery_only=${med_card_delivery_only}`
     document.getElementById('profile-completion-link').href = link
 }
