04945e9889965228194bc995f8af75660adf82e6
harishrongala <harishrongala@gmail.com>
2023-01-29 16:30:59 +0530

    thank you page

--- NAGE/paymentStatus.js
+++ NAGE/paymentStatus.js
@@ -25,19 +25,19 @@ function paymentSuccessful(selectedPlan, userEmailAddress) {
     document.getElementById('payment-successful-div').style.display = 'block';
     // Mobile Breakpoint
     if (window.screen.width <= 479) {
-        document.querySelector('.payment-successful').style.display = 'none';
-        document.querySelector('.payment-successful-mobile')?.style.display = 'flex';
+        // document.querySelector('.payment-successful').style.display = 'none';
+       // document.querySelector('.payment-successful-mobile').style.display = 'flex';
     } else {
-        document.querySelector('.payment-successful').style.display = 'flex';
-        document.querySelector('.payment-successful-mobile')?.style.display = 'none';
+        // document.querySelector('.payment-successful').style.display = 'flex';
+        // document.querySelector('.payment-successful-mobile').style.display = 'none';
     }
 }
 
 function pageExpired() {
     hideLoading();
     document.getElementById('payment-default-div').style.display = 'block';
-    document.querySelector('.payment-successful').style.display = 'none';
-    document.querySelector('.payment-successful-mobile').style.display = 'none';
+    // document.querySelector('.payment-successful').style.display = 'none';
+    // document.querySelector('.payment-successful-mobile').style.display = 'none';
 }
 
 const qs = new URLSearchParams(document.location.search);
