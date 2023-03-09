800bfdacb7d6e61399cdbb107cb3da15ae52ad14
harishrongala <harishrongala@gmail.com>
2023-03-09 20:34:32 +0530

    pain kit script added

---
+++ PainKit/createAccount.js
@@ -0,0 +1,41 @@
+function onload() {
+    const qs = new URLSearchParams(window.location.search)
+    const submission_id = qs.get('submission_id')
+    let supported_unions = ['iaff', 'btu', 'ethos']
+    let union_type = 'iaff'
+    supported_unions.forEach((union) => {
+        if (window.location.pathname.includes(`/${union}/`)) {
+            union_type = union
+        }
+    })
+    // iFrame event listener
+    console.log(`Adding iFrame message listener.`)
+    window.addEventListener('message', function(event) {
+        console.log("Message received from the child: ") // Message received from child
+        console.log(event.data)
+        const { zip, uid, thc_type_preference, med_card, med_card_number, redirect } = event.data
+        if (zip) {
+            const { med_card_delivery_only, rec_delivery_possible } = zip
+            this.localStorage.setItem('med_card_delivery_only', med_card_delivery_only)
+            this.localStorage.setItem('rec_delivery_possible', rec_delivery_possible)
+        }
+        if (uid || thc_type_preference || med_card || med_card_number) {
+            this.localStorage.setItem('uid', uid)
+            this.localStorage.setItem('thc_type_preference', thc_type_preference)
+            this.localStorage.setItem('med_card', med_card)
+            this.localStorage.setItem('med_card_number', med_card_number)
+        }
+        if (redirect) {
+            this.document.location.href = redirect
+        }
+    });
+
+    const _iframeDivId = 'JotFormIFrame-230167209377155'
+    document.getElementById(_iframeDivId).src =  `https://eo-care.web.app/pain-kit/order.html?submission_id=${submission_id}&union_type=${union_type}`
+    document.getElementById(_iframeDivId).style.display = ''
+    document.getElementById(_iframeDivId).style.height = '100%'
+    document.getElementById(_iframeDivId).style.width = '100%'
+}
+
+// setTimeout(onload, 800)
+onload()
