226361ecc3b5ef567e6d9b65e5046175ddf039f3
harishrongala <harishrongala@gmail.com>
2023-03-09 20:36:32 +0530

    pain kit script added

---
+++ PainKit/thankyou.js
@@ -0,0 +1,24 @@
+async function onload() {
+    let qs = new URLSearchParams(document.location.search);
+    let uid = qs.get('uid') || localStorage.getItem('uid')
+    let submission_id = qs.get('submission_id')
+    if (uid && submission_id) {
+        await saveProfileCompletionFormData(uid, submission_id)
+    }
+}
+
+async function saveProfileCompletionFormData(uid, submission_id) {
+    // 
+    // https://us-central1-eo-care.cloudfunctions.net
+    const resp = await fetch(`https://us-central1-eo-care.cloudfunctions.net/profileCompletion?uid=${uid}&submission_id=${submission_id}`, {
+          method: 'GET'
+    });
+    if (resp.ok && resp.status === 200) {
+      const res = await resp.json()
+      return Promise.resolve(res)
+    } else {
+      return Promise.reject(new Error(resp))
+    }
+ }
+
+ onload()
