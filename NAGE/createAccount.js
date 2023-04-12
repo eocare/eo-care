e93087bb6f1aa5d9c1ec6ccd54472f31d9969b29
harishrongala <harishrongala@gmail.com>
2023-04-12 22:06:00 +0530

    ayr account med_card_status

--- NAGE/createAccount.js
+++ NAGE/createAccount.js
@@ -14,8 +14,8 @@ function onload() {
         console.log("Message received from the child: ") // Message received from child
         console.log(event.data)
         const { zip, uid, thc_type_preference, med_card, med_card_number, redirect } = event.data
-        const { med_card_delivery_only, rec_delivery_possible } = zip
         if (zip) {
+            const { med_card_delivery_only, rec_delivery_possible } = zip
             this.localStorage.setItem('med_card_delivery_only', med_card_delivery_only)
             this.localStorage.setItem('rec_delivery_possible', rec_delivery_possible)
         }
@@ -37,6 +37,8 @@ function onload() {
                 } else {
                     med_card_status = "False"
                 }
+                const rec_delivery_possible = this.localStorage.getItem('rec_delivery_possible')
+                const med_card_delivery_only = this.localStorage.getItem('med_card_delivery_only')
                 const link = `https://eo.care/${union_type}/profile-completion?thc_plan_type=${thc_type_preference}&med_card_status=${med_card_status}&rec_delivery_possible=${rec_delivery_possible}&med_delivery_only=${med_card_delivery_only}&union=${union_type}`;
                 this.document.location.href = link
             } else {
